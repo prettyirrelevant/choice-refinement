d4936dc789791eca065189648e76635fd5f70749
Fionna <fionnacst@gmail.com>
2021-12-14 13:18:51 +0800

    Try to deploy on netlify

--- src/statics/PopFromBottomModal.tsx
+++ src/statics/PopFromBottomModal.tsx
@@ -70,7 +70,7 @@ const PopFromBottomModal = () => {
         dispatch({ type: "reset" });
       });
     },
-    [walletConnector]
+    [dispatch]
   );
 
   const setAccountsAtConnection = useCallback(
